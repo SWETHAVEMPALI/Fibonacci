bdae29fb146e7e4a72a29613f42f65dacfdd08e6
swetha-vempali <swethanaidu115@gmail.com>
2019-03-24 20:25:19 +0530

    Add basic test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .views import *
 # Create your tests here.
+
+class FibonacciApiTest(TestCase):
+	
+	def get_nth_number_Test(self):
+		nthNumber = views.get_nth_number(6)
+		self.assertIs(nthNumber,8)
